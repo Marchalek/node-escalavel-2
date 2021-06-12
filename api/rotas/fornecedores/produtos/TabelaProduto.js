00e731f731c2b9b6bdb2e2183d62b07f76a507b4
Marchalek <marchalek@hotmail.com>
2021-06-12 15:32:24 -0300

    M3 terminado

--- api/rotas/fornecedores/produtos/TabelaProduto.js
+++ api/rotas/fornecedores/produtos/TabelaProduto.js
@@ -1,10 +1,12 @@
 const Modelo = require('./ModeloTabelaProduto')
+const instancia = require ('../../../banco-de-dados')
 module.exports = {
     listar (idFornecedor) {
         return Modelo.findAll({
             where: {
                 fornecedor: idFornecedor
-            }
+            },
+            raw: true
         })
     },
 
@@ -18,6 +20,48 @@ module.exports = {
                 id: idProduto,
                 fornecedor: idFornecedor
             }
+        })
+    },
+
+    async pegarPorId (idProduto, idFornecedor) {
+        const encontrado = await Modelo.findOne({
+            where: {
+                id: idProduto,
+                fornecedor: idFornecedor
+            },
+            raw: true
+        })
+
+        if (!encontrado) {
+            throw new Error ('Produto não foi encontrado')
+        }
+
+        return encontrado
+    },
+
+    atualizar (dadosDoProduto, dadosParaAtualizar) {
+        return Modelo.update(
+            dadosParaAtualizar,
+            {
+                where: dadosDoProduto
+            }
+        )
+    },
+
+    subtrair (idProduto, idFornecedor, campo, quantidade) {
+        return instancia.transaction(async transacao => {
+            const produto = await Modelo.findOne ({
+                where: {
+                    id: idProduto,
+                    fornecedor: idFornecedor
+                }
+            })
+
+            produto[campo] = quantidade
+
+            await produto.save()
+
+            return produto
         })
     }
 }
